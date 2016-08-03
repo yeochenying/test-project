7e34a3ad921d02bfbf58089ffa34c640156a2728
boey89 <PohPoh.Jion@warnerchappell.com>
2016-08-03 16:55:15 +0800

    added unit test

---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by PohPohJion on 8/3/2016.
+ */
+public class MyClassTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+    @Test
+    public void cude() throws Exception {
+
+    }
+
+}
