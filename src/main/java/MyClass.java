622e1819eb083051259a1ee3cd49bc9eff516146
boey89 <PohPoh.Jion@warnerchappell.com>
2016-09-08 14:47:06 +0800

    conflict boey 1

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -7,7 +7,7 @@ public class MyClass {
 //		System.out.println(cude(a));
 		
 		MyClass my = new MyClass();
-		my.question2(4455);
+		my.question2(999);
 //		my.question3();
 //		my.question4();
 //		Khairul is here again 03
