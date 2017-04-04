e63fb1075a6edda1cb0839f49544d605d9e06a9e
Lee See Chien <lee.see-chien@hpe.com>
2017-04-05 00:02:33 +0800

    commit TCP-1236

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -7,7 +7,7 @@ public class MyClass {
 //		System.out.println(cude(a));
 		
 		MyClass my = new MyClass();
-		my.question2(111111);
+		my.question2(111112);
 //		my.question3();
 //		my.question4();
 //		Khairul is here again 03
